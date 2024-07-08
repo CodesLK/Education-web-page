eb57a53c3fc79a7b89b1b3af2f47fcf887a6d2ea
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-07-08 21:11:50 +0530

    Files are formatted properly

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
-import './component.css';
-import logo from '../Images/sliate.png';
+import React from "react";
+import "./component.css";
+import logo from "../Images/sliate.png";
 
-export default function Header(){
-    return(
-        <div className='header-container'>
-            <div className='logo'>
-                <img src={logo} alt="" srcset="" />
-            </div>
-           <div className='navigation'>
-            <ul>
-            <li>
-                <input type="radio" name="navigations" id="check-home"/>
-                <label htmlFor='check-home' className='nav-label'>Home</label>
-            </li>
-            <li>
-                <input type="radio" name="navigations" id="check-about" />
-                <label htmlFor='check-about'  className='nav-label'>About us</label>
-            </li>
-            <li>
-                <input type="radio" name="navigations" id="check-news"/>
-                <label htmlFor='check-news' className='nav-label'>News</label>
-            </li>
-            <li>
-                <input type="radio" name="navigations" id="check-courses" />
-                <label htmlFor='check-courses'  className='nav-label'>Courses</label>
-            </li> <li>
-                <input type="radio" name="navigations" id="check-research"/>
-                <label htmlFor='check-research' className='nav-label'>Research</label>
-            </li>
-            <li>
-                <input type="radio" name="navigations" id="check-contact" />
-                <label htmlFor='check-contact'  className='nav-label'>Contact us</label>
-            </li>
-            
-               
-
-            </ul>
-           </div>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="header-container">
+      <div className="logo">
+        <img src={logo} alt="" srcset="" />
+      </div>
+      <div className="navigation">
+        <ul>
+          <li>
+            <input type="radio" name="navigations" id="check-home" />
+            <label htmlFor="check-home" className="nav-label">
+              Home
+            </label>
+          </li>
+          <li>
+            <input type="radio" name="navigations" id="check-about" />
+            <label htmlFor="check-about" className="nav-label">
+              About us
+            </label>
+          </li>
+          <li>
+            <input type="radio" name="navigations" id="check-news" />
+            <label htmlFor="check-news" className="nav-label">
+              News
+            </label>
+          </li>
+          <li>
+            <input type="radio" name="navigations" id="check-courses" />
+            <label htmlFor="check-courses" className="nav-label">
+              Courses
+            </label>
+          </li>{" "}
+          <li>
+            <input type="radio" name="navigations" id="check-research" />
+            <label htmlFor="check-research" className="nav-label">
+              Research
+            </label>
+          </li>
+          <li>
+            <input type="radio" name="navigations" id="check-contact" />
+            <label htmlFor="check-contact" className="nav-label">
+              Contact us
+            </label>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
